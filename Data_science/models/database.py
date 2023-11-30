81ada0836f177cae2757f6d1c624a88d81828c1a
SofyaFin <151746034+SofyaFin@users.noreply.github.com>
2023-11-30 20:41:28 +0300

    Update database.py

--- Data_science/models/database.py
+++ Data_science/models/database.py
@@ -1,1 +1,13 @@
-# Кажется, что тут надо что-то написать
+from sqlalchemy import Integer, String, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+
+class Apples(Base):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    price: Mapped[int] = mapped_column(Integer, nullable=False)
+    mass: Mapped[int] = mapped_column(Integer, nullable=False)
+    sort: Mapped[str] = mapped_column(String, nullable=False)
+    time_of_harvest: Mapped[str] = mapped_column(DateTime, nullable=False)
+
+
