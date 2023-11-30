b1f91dd7666966f16f2f171c1c74192a631ed2f5
SofyaFin <151746034+SofyaFin@users.noreply.github.com>
2023-11-30 20:43:01 +0300

    Update database.py

--- Data_science/models/database.py
+++ Data_science/models/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Integer, String, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
-
+from datetime import datetime
 from .base import Base
 
 class Apples(Base):
@@ -8,6 +8,5 @@ class Apples(Base):
     price: Mapped[int] = mapped_column(Integer, nullable=False)
     mass: Mapped[int] = mapped_column(Integer, nullable=False)
     sort: Mapped[str] = mapped_column(String, nullable=False)
-    time_of_harvest: Mapped[str] = mapped_column(DateTime, nullable=False)
-
+    time_of_harvest: Mapped[datetime] = mapped_column(DateTime, nullable=False)
 
